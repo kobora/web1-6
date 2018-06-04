509af88ae09a8ca7d061798b8cd68d53ec116eff
Unknown <hita0718-yuki10maga-kira.6978@i.softbank.jp>
2018-06-04 18:54:32 +0900

    はじめてのコミット

---
+++ CatEscape/Assets/PleyerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PleyerController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);
+
+    }
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0);
+    }
+}
